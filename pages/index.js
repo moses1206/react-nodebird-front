b4d2e6e0059b56952941c3ddb8b0df072a34abd7
moses1206 <mose23@naver.com>
2021-05-27 23:04:48 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AppLayout from "../components/AppLayout";
+
+const Home = () => {
+  return (
+    <div>
+      <AppLayout />
+      Hello, Next!!
+    </div>
+  );
+};
+export default Home;
---
+++ pages/profile.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AppLayout from "../components/AppLayout";
+
+const Profile = () => {
+  return <AppLayout>MyProfile</AppLayout>;
+};
+
+export default Profile;
